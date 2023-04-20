e32a34b3e81ffa7c37be6f4a261a39b5a4cb40db
Paulo Eduardo <68037168+pauloeduard0@users.noreply.github.com>
2023-04-20 13:19:43 -0300

    modificações docker e stockcontroller

--- src/main/java/com/inatel/quotationmanagement/controller/StockController.java
+++ src/main/java/com/inatel/quotationmanagement/controller/StockController.java
@@ -1,16 +1,25 @@
 package com.inatel.quotationmanagement.controller;
 
 import com.inatel.quotationmanagement.model.dto.StockQuoteDto;
+import com.inatel.quotationmanagement.model.entities.Quote;
+import com.inatel.quotationmanagement.model.entities.Stock;
+import com.inatel.quotationmanagement.repository.StockRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/stock")
 public class StockController {
+
+    @Autowired
+    private StockRepository repository;
     @PostMapping
     public void cadastrar(@RequestBody StockQuoteDto stockQuoteDto){
-        System.out.println(stockQuoteDto);
+        repository.save(new Stock(stockQuoteDto));
     }
 }
