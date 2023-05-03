3e7cfeac27df4a71f469db42bbd26f64f3b03f71
Paulo Eduardo <68037168+pauloeduard0@users.noreply.github.com>
2023-05-03 11:55:05 -0300

    feat: implementacao da classe para controle de excecoes

--- src/main/java/com/inatel/quotationmanagement/handler/ControllerExceptionHandler.java
+++ src/main/java/com/inatel/quotationmanagement/handler/ControllerExceptionHandler.java
@@ -1,8 +1,13 @@
 package com.inatel.quotationmanagement.handler;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.inatel.quotationmanagement.exception.StockManagerConnectionException;
 import com.inatel.quotationmanagement.exception.StockNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.JDBCConnectionException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -20,6 +25,60 @@ public class ControllerExceptionHandler {
         return Error.builder()
                 .httpStatusCode(HttpStatus.NOT_FOUND)
                 .message(stockNotFoundException.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(StockManagerConnectionException.class)
+    @ResponseStatus(value = HttpStatus.SERVICE_UNAVAILABLE)
+    public Error stockManagerConnectionException(StockManagerConnectionException stockManagerConnectionException){
+        return Error.builder()
+                .httpStatusCode(HttpStatus.SERVICE_UNAVAILABLE)
+                .message(stockManagerConnectionException.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(InvalidFormatException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public Error invalidFormatExcepetion(InvalidFormatException invalidFormatException){
+        return Error.builder()
+                .httpStatusCode(HttpStatus.BAD_REQUEST)
+                .message(invalidFormatException.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(JsonMappingException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public Error jsonMappingException(JsonMappingException jsonMappingException){
+        return Error.builder()
+                .httpStatusCode(HttpStatus.BAD_REQUEST)
+                .message(jsonMappingException.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public Error methodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException){
+        return Error.builder()
+                .httpStatusCode(HttpStatus.BAD_REQUEST)
+                .message(methodArgumentNotValidException.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(JDBCConnectionException.class)
+    @ResponseStatus(value = HttpStatus.SERVICE_UNAVAILABLE)
+    public Error jDBCConnectionException(JDBCConnectionException jDBCConnectionException){
+        return Error.builder()
+                .httpStatusCode(HttpStatus.SERVICE_UNAVAILABLE)
+                .message(jDBCConnectionException.getMessage())
+                .build();
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public Error nullPointerException(NullPointerException nullPointerException){
+        return Error.builder()
+                .httpStatusCode(HttpStatus.INTERNAL_SERVER_ERROR)
+                .message(nullPointerException.getMessage())
                 .build();
     }
 }
