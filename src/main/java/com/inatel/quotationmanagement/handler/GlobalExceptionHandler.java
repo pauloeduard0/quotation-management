e1fdc47d541e7fe296829c5e3bc7aa65fe87ac79
Paulo Eduardo <68037168+pauloeduard0@users.noreply.github.com>
2023-05-10 17:45:19 -0300

    refactor: adicionando algumas dependencias e correcoes o handler

--- src/main/java/com/inatel/quotationmanagement/handler/GlobalExceptionHandler.java
+++ src/main/java/com/inatel/quotationmanagement/handler/GlobalExceptionHandler.java
@@ -7,7 +7,6 @@ import com.inatel.quotationmanagement.exception.StockNotFoundException;
 import org.hibernate.exception.JDBCConnectionException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
