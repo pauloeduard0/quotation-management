696d6c4543a1c2f5bc9fceaaf9df45812b8b2020
Paulo Eduardo <68037168+pauloeduard0@users.noreply.github.com>
2023-05-18 12:04:40 -0300

    refactor: correcao dos testes para utilizacao do webtestflux

--- src/test/java/com/inatel/quotationmanagement/controller/StockQuoteControllerTest.java
+++ src/test/java/com/inatel/quotationmanagement/controller/StockQuoteControllerTest.java
@@ -3,26 +3,14 @@ package com.inatel.quotationmanagement.controller;
 import com.inatel.quotationmanagement.model.dto.StockQuoteDto;
 import com.inatel.quotationmanagement.service.StockQuoteService;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -30,75 +18,59 @@ import java.time.LocalDate;
 import java.util.Collections;
 
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(StockQuoteController.class)
-@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 @ActiveProfiles("test")
 class StockQuoteControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    private WebTestClient webTestClient;
 
-    @MockBean
+    @Autowired
     private StockQuoteService stockQuoteService;
 
-    @BeforeEach
-    public void setUp() {
-
-    }
-
     @Test
-    void givenNoParameters_whenGetAllStockQuote_thenReturnHttpStatus200AndEmptyStockQuoteDtoList() throws Exception {
-
-        Mockito.when(stockQuoteService.getAllStockQuote(Mockito.any(Pageable.class)))
-                .thenReturn(new PageImpl<>(Collections.emptyList()));
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/quote")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content").isArray())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content.length()").value(0));
-    }
-
-    @Test
-    void givenStockId_whenGetQuotes_thenReturnHttpStatus200AndEmptyStockQuoteDtoList() throws Exception {
-
-        Mockito.when(stockQuoteService.getStockQuoteByStockId(Mockito.eq("petr4"), Mockito.any(Pageable.class)))
-                .thenReturn(new PageImpl<>(Collections.emptyList()));
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/quote/{stockId}", "petr4")
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content").isArray())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.content.length()").value(0));
-    }
-
-    @Test
-    void givenStockQuoteDto_whenSaveQuote_thenReturnHttpStatus201AndSavedStockQuoteDto() throws Exception {
+    void givenStockQuoteDto_whenSaveQuote_thenReturnHttpStatus201AndSavedStockQuoteDto() {
 
         StockQuoteDto stockQuoteDto = StockQuoteDto.builder()
                 .stockId("petr4")
                 .quotes(Collections.singletonMap(LocalDate.now(), BigDecimal.valueOf(20L)))
                 .build();
-        Mockito.when(stockQuoteService.saveStockQuote(Mockito.any(StockQuoteDto.class)))
-                .thenReturn(stockQuoteDto);
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/quote")
-                        .content("{ \"stockId\": \"petr4\", \"quotes\": { \"2023-05-15\": 20 } }")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isCreated())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.stockId").value("petr4"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.quotes").isNotEmpty());
+        webTestClient.post()
+                .uri("/quote")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue(stockQuoteDto)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody()
+                .jsonPath("$.stockId").isEqualTo(stockQuoteDto.stockId())
+                .jsonPath("$.quotes").isNotEmpty();
     }
 
     @Test
-    void givenInvalidFormatStockQuoteDto_whenSaveQuote_thenReturnHttpStatus400() throws Exception {
+    void givenInvalidFormatStockQuoteDto_whenSaveQuote_thenReturnHttpStatus400() {
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/quote")
-                        .content("{ \"stockId\": \"Petr4\", \"quotes\": { \"2023-05-15\": \"invalid\" } }")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        webTestClient.post()
+                .uri("/quote")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .bodyValue("{ \"stockId\": \"Petr4\", \"quotes\": { \"2023-05-15\": \"invalid\" } }")
+                .exchange()
+                .expectStatus().isBadRequest();
     }
+
+    @Test
+    void givenStockId_whenGetQuotes_thenReturnHttpStatus200AndEmptyStockQuoteDtoList() {
+
+        webTestClient.get()
+                .uri("/quote/{stockId}", "petr4")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.content").isArray();
+    }
+    
 }
