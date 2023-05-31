c52fbb027611b87973664d6463afe037a4426ab9
Paulo Eduardo <68037168+pauloeduard0@users.noreply.github.com>
2023-05-31 11:04:57 -0300

    feat: criacao do teste para a validacao

---
+++ src/test/java/com/inatel/quotationmanagement/service/validation/DefaultStockValidatorTest.java
@@ -0,0 +1,40 @@
+package com.inatel.quotationmanagement.service.validation;
+
+import com.inatel.quotationmanagement.adapter.StockAdapter;
+import com.inatel.quotationmanagement.exception.StockNotFoundException;
+import com.inatel.quotationmanagement.model.entities.StockQuote;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(MockitoJUnitRunner.class)
+class DefaultStockValidatorTest {
+
+    private StockAdapter stockAdapter;
+    private DefaultStockValidator stockValidator;
+
+    @BeforeEach
+    public void setUp() {
+        stockAdapter = Mockito.mock(StockAdapter.class);
+        stockValidator = new DefaultStockValidator(stockAdapter);
+    }
+
+    @Test
+    void testIsValid_StockNotFound() {
+        Mockito.when(stockAdapter.getAllStock()).thenReturn(Collections.emptyList());
+
+        StockQuote stockQuote = new StockQuote();
+        stockQuote.setStockId("stockId");
+
+        assertThrows(StockNotFoundException.class, () -> stockValidator.isValid(stockQuote));
+    }
+}
