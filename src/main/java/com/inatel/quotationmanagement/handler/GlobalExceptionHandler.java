9120f23f9918d986c71451b05e3e175fc7d2d9a8
Paulo Eduardo <68037168+pauloeduard0@users.noreply.github.com>
2023-05-31 13:48:17 -0300

    refactor: criacao do metodo methodargumentnotvalid e exclusao do nullpointerexception

--- src/main/java/com/inatel/quotationmanagement/handler/GlobalExceptionHandler.java
+++ src/main/java/com/inatel/quotationmanagement/handler/GlobalExceptionHandler.java
@@ -7,16 +7,34 @@ import com.inatel.quotationmanagement.exception.StockNotFoundException;
 import org.hibernate.exception.JDBCConnectionException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.support.WebExchangeBindException;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public List<ProblemDetail> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ArrayList<ProblemDetail> problemDetailsList = new ArrayList<>();
+        e.getFieldErrors().forEach(fieldError -> {
+            ProblemDetail problemDetail = ProblemDetail.
+                    forStatusAndDetail(HttpStatus.BAD_REQUEST, Objects.requireNonNull(fieldError.getDefaultMessage()));
+            problemDetail.setTitle("Method Argument Not Valid Exception");
+            problemDetail.setType(URI.create("https://api.quotationmanagement.com/errors/bad-request"));
+            problemDetailsList.add(problemDetail);
+        });
+
+        return problemDetailsList;
+    }
 
     @ExceptionHandler(StockNotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
@@ -32,7 +50,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ProblemDetail handleStockManagerConnectionException(StockManagerConnectionException e) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.SERVICE_UNAVAILABLE, e.getMessage());
         problemDetail.setTitle("Stock Manager Connection Exception");
-        problemDetail.setType(URI.create("https://api.quotationmanagement.com/errors/connection-exception"));
+        problemDetail.setType(URI.create("https://api.quotationmanagement.com/errors/service-unavailable"));
         return problemDetail;
     }
 
@@ -41,7 +59,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ProblemDetail handleInvalidFormatException(InvalidFormatException e) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
         problemDetail.setTitle("Invalid Format Exception");
-        problemDetail.setType(URI.create("https://api.quotationmanagement.com/errors/invalid-format-exception"));
+        problemDetail.setType(URI.create("https://api.quotationmanagement.com/errors/bad-request"));
         return problemDetail;
     }
 
@@ -50,7 +68,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ProblemDetail handleJsonMappingException(JsonMappingException e) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
         problemDetail.setTitle("Json Mapping Exception");
-        problemDetail.setType(URI.create("https://api.bookmarks.com/errors/json-mapping-exception"));
+        problemDetail.setType(URI.create("https://api.bookmarks.com/errors/bad-request"));
         return problemDetail;
     }
 
@@ -59,7 +77,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ProblemDetail handleWebExchangeBindException(WebExchangeBindException e) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, e.getMessage());
         problemDetail.setTitle("Web Exchange Bind Exception");
-        problemDetail.setType(URI.create("https://api.bookmarks.com/errors/web-exchange-bind-exception"));
+        problemDetail.setType(URI.create("https://api.bookmarks.com/errors/bad-request"));
         return problemDetail;
     }
 
@@ -68,16 +86,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ProblemDetail handleJDBCConnectionException(JDBCConnectionException e) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.SERVICE_UNAVAILABLE, e.getMessage());
         problemDetail.setTitle("JDBC Connection Exception");
-        problemDetail.setType(URI.create("https://api.bookmarks.com/errors/jdbc-connection-exception"));
-        return problemDetail;
-    }
-
-    @ExceptionHandler(NullPointerException.class)
-    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
-    public ProblemDetail handleNullPointerException(NullPointerException e) {
-        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-        problemDetail.setTitle("Null Pointer Exception");
-        problemDetail.setType(URI.create("https://api.bookmarks.com/errors/null-pointer-exception"));
+        problemDetail.setType(URI.create("https://api.bookmarks.com/errors/service-unavailable"));
         return problemDetail;
     }
 
